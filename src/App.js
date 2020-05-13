2f3ebe044df904cdd7d14f4294a5912c7b0c944b
GaryZCLau <garyzclau@gmail.com>
2020-05-13 18:56:20 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,38 @@ import Header from './components/Header'
 import ToyForm from './components/ToyForm'
 import ToyContainer from './components/ToyContainer'
 
-import data from './data'
-
 
 class App extends React.Component{
 
   state = {
-    display: false
+    display: false,
+    toyList: []
+  }
+
+  // READ
+  componentDidMount(){
+    fetch("http://localhost:3000/toys")
+      .then(r => r.json())
+      .then((allLists) => {
+        this.setState({
+          toyList: allLists
+        })
+      })
+  }
+
+  handleCreate = (newToy) => {
+    fetch("http://localhost:3000/toys", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(newToy)
+    }).then(r => r.json()).then((newMadeToy) => {
+      let copyOfMasterToyList = [...this.state.toyList, newMadeToy]
+      this.setState({
+        toyList: copyOfMasterToyList
+      })
+    })
   }
 
   handleClick = () => {
@@ -21,20 +46,55 @@ class App extends React.Component{
     })
   }
 
+  handleDelete = (idOfToy) => {
+    fetch(`http://localhost:3000/toys/${idOfToy}`, {
+      method: "DELETE"
+    })
+    .then(r => r.json())
+    .then((deletedToy) => {
+      let copyOfToyList = this.state.toyList.filter((singleToy) => {
+        return singleToy.id !== idOfToy
+      })
+      this.setState({toyList: copyOfToyList})
+    })
+  }
+
+  handleUpdate = (singleToy) => {
+    fetch(`http://localhost:3000/toys/${singleToy.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify({
+        likes: singleToy.likes + 1
+      })
+    }).then(r => r.json())
+    .then((updatedToy) => {
+      let copyOfMasterToyList = this.state.toyList.map((oneToy)=>{
+        return (oneToy.id === updatedToy.id ? updatedToy : oneToy)
+      })
+      this.setState({
+        toyList: copyOfMasterToyList
+      })
+    })
+  }
+
   render(){
     return (
       <>
         <Header/>
-        { this.state.display
-            ?
-          <ToyForm/>
-            :
-          null
+        { this.state.display ? <ToyForm
+          handleCreate = {this.handleCreate}
+        /> : null
         }
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer
+        toyList={this.state.toyList}
+        handleDelete={this.handleDelete}
+        handleUpdate={this.handleUpdate}
+        />
       </>
     );
   }
--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 
+
 class ToyForm extends Component {
+  
+  state = {
+    name: "",
+    image: "",
+    likes: 0
+  }
+
+  handleAllInput = (evt) => {
+    let {name, value} = evt.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = (evt) => {
+    evt.preventDefault()
+    this.props.handleCreate(this.state)
+  
+  }
 
   render() {
     return (
       <div className="container">
-        <form className="add-toy-form">
+        <form className="add-toy-form" onSubmit={this.handleSubmit}>
           <h3>Create a toy!</h3>
-          <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
+          <input type="text" name="name" placeholder="Enter a toy's name..." value={this.state.name} onChange={this.handleAllInput} className="input-text"/>
           <br/>
-          <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
+          <input type="text" name="image" placeholder="Enter a toy's image URL..."  value={this.state.image} onChange={this.handleAllInput} className="input-text"/>
           <br/>
           <input type="submit" name="submit" value="Create New Toy" className="submit"/>
         </form>
