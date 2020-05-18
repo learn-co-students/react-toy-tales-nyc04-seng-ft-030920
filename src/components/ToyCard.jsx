5aabd7452d283d20b11d0185d67e228803376d8f
StephanieZou1 <stephanie.zou3@gmail.com>
2020-05-18 09:38:39 -0400

    Done.

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 
 class ToyCard extends Component {
 
+  handleDelete = (evt) => {
+    this.props.deleteAToy(this.props.toy.id)  
+  }
+
+  handleLike = (evt) => {
+    let newLikesValue = this.props.toy.likes + 1
+    this.props.updateLikes(this.props.toy.id, newLikesValue)
+  }
+
   render() {
+    let {name, image, likes} = this.props.toy
     return (
       <div className="card">
-        <h2>{'' /* Toy's Name */}</h2>
-        <img src={'' /* Toy's Image */} alt={/* Toy's Name */} className="toy-avatar" />
-        <p>{'' /* Toy's Likes */} Likes </p>
-        <button className="like-btn">Like {'<3'}</button>
-        <button className="del-btn">Donate to GoodWill</button>
+        <h2>{name}</h2>
+        <img src={image} alt={name} className="toy-avatar" />
+        <p> {likes} Likes </p>
+        <button className="like-btn" onClick={this.handleLike}>Like {'<3'}</button>
+        <button className="del-btn" onClick={this.handleDelete}>Donate to GoodWill</button>
       </div>
     );
   }
--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -2,14 +2,43 @@ import React, { Component } from 'react';
 
 class ToyForm extends Component {
 
+  state = {
+    toy_name: "steph",
+    toy_image: "url"
+  }
+
+  handleAllInput = (evt) => {
+    let {name, value} = evt.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = (evt) => {
+    evt.preventDefault()
+    this.props.addAToy(this.state)
+  }
+
   render() {
     return (
       <div className="container">
-        <form className="add-toy-form">
+        <form className="add-toy-form" onSubmit={this.handleSubmit}>
           <h3>Create a toy!</h3>
-          <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
+          <input 
+            type="text" 
+            name="toy_name" 
+            placeholder="Enter a toy's name..." 
+            className="input-text"
+            value={this.state.toy_name}
+            onChange={this.handleAllInput}/>
           <br/>
-          <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
+          <input 
+            type="text" 
+            name="toy_image" 
+            placeholder="Enter a toy's image URL..." 
+            className="input-text"
+            value={this.state.toy_image}
+            onChange={this.handleAllInput}/>
           <br/>
           <input type="submit" name="submit" value="Create New Toy" className="submit"/>
         </form>
